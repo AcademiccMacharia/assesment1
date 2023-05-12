75ddfe63ca9d0c15ddeb25a86aca6d2dcdc42ed9
AcademiccMacharia <machariab19@gmail.com>
2023-05-12 11:25:09 +0300

    explanation of the result from a qiven equation

---
+++ question5.js
@@ -0,0 +1,6 @@
+// let sum = 5+ 5+"5";
+
+// console.log(sum);
+
+let answer = "Given the above the log will return 105, this is because the five being enclosed using quotes means it will be interpreted as a string rather than as an interger therefore resulting in only the addition of the first two fives happening then the last five is concanated hence the result is 105.";
+console.log(answer)
